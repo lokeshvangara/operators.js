ea2ea4b16088e479df76c614afd9d6ea6277e910
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 15:02:48 +0530

    Create Arthmetic-operators.js

---
+++ Arthmetic-operators.js
@@ -0,0 +1,12 @@
+//Arithmetic operators
+var x= 4
+var y= 5
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(++x);
+console.log(--y);
+console.log(x**7);
