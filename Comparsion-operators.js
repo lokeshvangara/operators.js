b0fe60631bcc2bfd4582fe85692e3f69989f79a8
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 15:04:12 +0530

    Create Comparsion-operators.js

---
+++ Comparsion-operators.js
@@ -0,0 +1,12 @@
+// Comparsion operators
+
+var x=6
+var y=4
+
+console.log(x<=y);
+console.log(x>=y);
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+console.log(x<y);
+console.log(x>y);
