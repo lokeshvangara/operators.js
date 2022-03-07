9b8a01dca723423306e7cb1b290bea13cf0659f9
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 15:03:37 +0530

    Create Assignment-operators.js

---
+++ Assignment-operators.js
@@ -0,0 +1,15 @@
+// Assignment operators
+
+var x= 3
+var y= 7
+
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x%=y);
+console.log(x<<=y);
+console.log(x>>=y);
+console.log(x&=y);
+console.log(x^=y);
+console.log(x**=y);
