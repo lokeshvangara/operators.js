04e237d6bc3854b562e2ca27398af027905420de
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 15:05:43 +0530

    Create Bitwise & string operators.js

---
+++ Bitwise & string operators.js
@@ -0,0 +1,17 @@
+//Bitwise operators
+
+var x=4
+var y=5
+
+console.log(x&y);
+console.log(x^y);
+console.log(x|y);
+console.log(x<<y);
+console.log(x>>y);
+console.log(x>>>y);
+
+// String operators
+
+var x = 2
+
+document.write(x +"2"+2)
